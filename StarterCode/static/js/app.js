c17cf134effb33597b2e101554c3c21ea247805f
TPhillips27 <69609977+TPhillips27@users.noreply.github.com>
2020-12-13 19:32:22 -0500

    update

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -7,55 +7,66 @@ var tbody = d3.select('tbody')
 console.log(tableData);
 
 
-// Step 1: Loop Through `data` and console.log each weather report object
+// Step 1: Loop Through `data` and console.log each ufo object
 tableData.forEach(function(ufo) {
     console.log(ufo);
 });
 
-// Step 2:  Use d3 to append one table row `tr` for each weather report object
+// Use d3 to update each cell's text with ufo values (date/time, city, state, country, shape, comment)
 tableData.forEach(function(ufo){
-    console.log(ufo);
-    var row = tbody.append('tr');
-});
-
-// Step 3:  Use `Object.entries` to console.log each weather report value
-tableData.forEach(function(ufo) {
-    console.log(ufo);
-    var row = tbody.append('tr');
-
-    Object.entries(ufo).forEach(function([key, value]) {
-        console.log(key, value);
-        });
-});
-
-// Step 4: Use d3 to append 1 cell per ufo sighting value (date/time, city, state, country, shape, comment)
-tableData.forEach(function(ufo) {
-    console.log(ufo);
-    var row = tbody.append('tr');
-
-    Object.entries(ufo).forEach(function([key, value]){
-        console.log(key, value);
-        //Append a cell to the row for each value in the ufo data object
-        var cell = row.append('td');
-    });
-});
-
-// Step 5: Use d3 to update each cell's text with ufo values (date/time, city, state, country, shape, comment)
-tableData.forEach(function(ufo){
-    console.log(ufo);
+    //console.log(ufo);
     var row = tbody.append('tr');
     Object.entries(ufo).forEach(function([key, value]){
-        console.log(key, value);
+        //console.log(key, value);
         var cell = row.append('td');
         cell.text(value);
     });
 });
 
-// BONUS: Refactor to use Arrow Functions!
-// data.forEach((ufo) => {
-//     var row = tbody.append("tr");
-//     Object.entries(ufo).forEach(([key, value]) => {
-//       var cell = row.append("td");
-//       cell.text(value);
-//     });
-//   });
+//Set up filter button
+var button = d3.select('#filter-btn');
+
+// Select the form. If # is part of form then you have to click as opposed to hitting enter too
+var form = d3.select('form');
+
+// Create event handlers
+button.on('click', runEnter);
+form.on('submit', runEnter);
+
+//Complete the event handler function for the form
+function runEnter(){
+
+     // Prevent the page from refreshing
+  d3.event.preventDefault();
+  
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+  
+  var filteredData = tableData.filter(entered => entered.datetime === inputValue);
+
+  console.log(filteredData);
+
+  tbody.html('')
+
+  filteredData.forEach(function(ufo){
+    //console.log(ufo);
+    var row = tbody.append('tr');
+    Object.entries(ufo).forEach(function([key, value]){
+        //console.log(key, value);
+        var cell = row.append('td');
+        cell.text(value);
+    });
+});
+};
+
+
+
+
+
+
+
